35432a902a6ba77a099889ad2a34626ea1429cc0
Billybobchops <chlanda52@gmail.com>
2023-10-08 15:35:49 -0400

    make style changes for kebab menus

--- components/UI/BudgetItem.js
+++ components/UI/BudgetItem.js
@@ -5,119 +5,121 @@ import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import { Draggable } from 'react-beautiful-dnd';
 
 const BudgetItem = ({
-  title,
-  index,
-  budgetedAmount,
-  date,
-  tabID,
-  accordionType,
+    title,
+    index,
+    budgetedAmount,
+    date,
+    tabID,
+    accordionType,
 }) => {
-  // className={`${[classes.container, classes[`${snapshot.isDragging && "backgroundDrag"}`],].join(" ")}`}
-  const daySlice = date.slice(-2);
-  const today = new Date().getDate();
-  const dateString = +daySlice === today ? 'Today' : date;
-  // const displayDate = `${
-  //   dateString === 'Today' ? `Bills` : 'Bills on'
-  // } ${dateString}`;
+    // className={`${[classes.primaryContainer, classes[`${snapshot.isDragging && "backgroundDrag"}`],].join(" ")}`}
+    const daySlice = date.slice(-2);
+    const today = new Date().getDate();
+    const dateString = +daySlice === today ? 'Today' : date;
+    // const displayDate = `${
+    //   dateString === 'Today' ? `Bills` : 'Bills on'
+    // } ${dateString}`;
 
-  const displayDate = `${dateString === 'Today' ? `Today` : date}`;
+    const displayDate = `${dateString === 'Today' ? `Today` : date}`;
 
-  const expenses = useSelector((state) => state.expenses.entities);
-  let balanceClass = null;
-  let balanceString = null;
-  let spent = 0;
+    const expenses = useSelector((state) => state.expenses.entities);
+    let balanceClass = null;
+    let balanceString = null;
+    let spent = 0;
 
-  // would it be better to do this via item-slice once on fetch?
-  // instead of every time the accordion opens/closes...
-  if (Object.values(expenses).length !== 0) {
-    Object.values(expenses).map((expense) => {
-      if (title === expense.title) spent += expense.amount;
-      // console.log('func running'); // 20 freaking times for 5 items! Must fix.
-      // runs for each item for EVERY SINGLE EXPENSE that's in state....
-      // ADD THIS TO useEffect ya dingus!
-    });
-  }
+    // would it be better to do this via item-slice once on fetch?
+    // instead of every time the accordion opens/closes...
+    if (Object.values(expenses).length !== 0) {
+        Object.values(expenses).map((expense) => {
+            if (title === expense.title) spent += expense.amount;
+            // console.log('func running'); // 20 freaking times for 5 items! Must fix.
+            // runs for each item for EVERY SINGLE EXPENSE that's in state....
+            // ADD THIS TO useEffect ya dingus!
+        });
+    }
 
-  if (spent === budgetedAmount) {
-    balanceClass = 'balanced';
-    balanceString = '✅ Balanced';
-  }
-  if (spent > budgetedAmount) {
-    balanceClass = 'over';
-    balanceString = '🔺 Over';
-  }
-  if (spent < budgetedAmount) {
-    balanceClass = 'under';
-    balanceString = '🎉 Under';
-  }
-  if (tabID === 'Annual') budgetedAmount = budgetedAmount * 12;
+    if (spent === budgetedAmount) {
+        balanceClass = 'balanced';
+        balanceString = '✅ Balanced';
+    }
 
-  return (
-    <>
-      <Draggable key={title} draggableId={title} index={index}>
-        {(provided, snapshot) => {
-          const style = `${[
-            classes.container,
-            classes[`${snapshot.isDragging && 'backgroundDrag'}`],
-						classes[`${accordionType === 'planner' && 'plannerMargin'}`]
-          ].join(' ')}`;
+    if (spent > budgetedAmount) {
+        balanceClass = 'over';
+        balanceString = '🔺 Over';
+    }
 
-          return (
-            <li
-              ref={provided.innerRef}
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
-              className={style}
-            >
-              <table className={classes.table}>
-                <tbody>
-                  <tr className={classes.initialRow}>
-                    <td className={classes.emptyCell}></td>
-                    <td className={classes.col1}>{title}</td>
-                    <td className={classes.col2}>
-                      <div>{displayDate}</div>
-                    </td>
-                    <td className={classes.col3}>
-                      <div className={classes.flex}>
-                        <div>
-                          <span className={classes.bold}>Spent</span> ${spent}
-                        </div>
-                        <div className={classes.slash}>/</div>
-                        <div>${budgetedAmount}</div>
-                      </div>
-                    </td>
-                    <td className={classes.col4}>
-                      <div className={classes[balanceClass]}>
-                        {balanceString}!
-                      </div>
-                    </td>
-                    <td className={classes.col5}>
-                      {/* this ellipsis needs to be wrapped by <button> to be interactive */}
-                      <FontAwesomeIcon icon={faEllipsisH} />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+    if (spent < budgetedAmount) {
+        balanceClass = 'under';
+        balanceString = '🎉 Under';
+    }
 
-              <div className={classes.secondaryRow}>
-                <div className={classes.bold}>{title}</div>
-                <div className={classes.date}>{displayDate}</div>
-                <div className={classes.flex}>
-                  <div className={classes.spent}>${spent}</div>
-                  <div className={classes.slash}>/</div>
-                  <div className={classes.budgeted}>${budgetedAmount}</div>
-                </div>
-                <div className={classes[balanceClass]}>{balanceString}!</div>
-                <div className={classes.options}>
-                  <FontAwesomeIcon icon={faEllipsisH} />
-                </div>
-              </div>
-            </li>
-          );
-        }}
-      </Draggable>
-    </>
-  );
+    if (tabID === 'Annual') budgetedAmount = budgetedAmount * 12;
+
+    const displayKebabOptions = () => {
+        console.log('Function separate oh yeah');
+    };
+
+    return (
+        <Draggable key={title} draggableId={title} index={index}>
+            {(provided, snapshot) => {
+                const style = `${[
+                    classes.primaryContainer,
+                    classes[`${snapshot.isDragging && 'backgroundDrag'}`],
+                    classes[
+                        `${accordionType === 'planner' && 'plannerMargin'}`
+                    ],
+                ].join(' ')}`;
+
+                return (
+                    <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                    >
+						<div className={classes.parentContainer}>
+							<div className={style}>
+								<div className={classes.secondaryContainer}>
+									<div className={classes.title}>{title}</div>
+
+									<div className={classes.date}>
+										<div>{displayDate}</div>
+									</div>
+
+									<div className={classes.spent}>
+										<div className={classes.flex}>
+											<div>
+												<span className={classes.bold}>
+													Spent
+												</span>{' '}
+												${spent}
+											</div>
+
+											<div className={classes.slash}>/</div>
+
+											<div>${budgetedAmount}</div>
+										</div>
+									</div>
+
+									<div className={classes.chip}>
+										<div className={classes[balanceClass]}>
+											{balanceString}!
+										</div>
+									</div>
+								</div>
+								
+								<button
+									className={classes.kebab}
+									onClick={displayKebabOptions}
+								>
+									<FontAwesomeIcon icon={faEllipsisH} />
+								</button>
+							</div>
+						</div>
+                    </li>
+                );
+            }}
+        </Draggable>
+    );
 };
 
 export default BudgetItem;
--- components/UI/CategoryAccordion.js
+++ components/UI/CategoryAccordion.js
@@ -3,174 +3,204 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faPlus,
-  faMinus,
-  faEllipsisH,
+    faPlus,
+    faMinus,
+    faEllipsisH,
 } from '@fortawesome/free-solid-svg-icons';
 import BudgetItem from './BudgetItem';
 import { Droppable } from 'react-beautiful-dnd';
 
 const CategoryAccordion = ({
-  categoryTitle,
-  percent,
-  budgetedTotal,
-  totalIncome,
-  spent,
-  items,
-  tabID,
+    categoryTitle,
+    percent,
+    budgetedTotal,
+    totalIncome,
+    spent,
+    items,
+    tabID,
 }) => {
-  const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
-  let budgeted = budgetedTotal !== undefined ? +budgetedTotal.toFixed(2) : 0;
+    let budgeted = budgetedTotal !== undefined ? +budgetedTotal.toFixed(2) : 0;
 
-  if (tabID === 'Annual') {
-    budgeted = +(budgeted * 12).toFixed(2);
-    totalIncome = totalIncome * 12;
-  }
+    if (tabID === 'Annual') {
+        budgeted = +(budgeted * 12).toFixed(2);
+        totalIncome = totalIncome * 12;
+    }
 
-  const percentDisplay =
-    percent * 100 <= 1 ? (
-      <span className={classes.percentage}>{'< 1%'}</span>
+    const percentDisplay =
+        percent * 100 <= 1 ? (
+            <span className={classes.percentage}>{'< 1%'}</span>
+        ) : (
+            <span className={classes.percentage}>
+                {(percent * 100).toFixed(0)}% of Income
+            </span>
+        );
+
+    let balanceClass = null;
+    let balanceString = null;
+
+    if (spent === budgeted) {
+        balanceClass = 'balanced';
+        balanceString = `Balanced!`;
+    }
+
+    if (spent > budgeted) {
+        balanceClass = 'over';
+        let num =
+            (spent - budgeted) % 1 === 0
+                ? spent - budgeted
+                : (spent - budgeted).toFixed(2);
+        balanceString = `$${num.toLocaleString()} Over`;
+    }
+
+    if (spent < budgeted) {
+        balanceClass = 'under';
+        let num =
+            (budgeted - spent) % 1 === 0
+                ? budgeted - spent
+                : +(budgeted - spent).toFixed(2);
+        balanceString = `$${num.toLocaleString()} Under`;
+    }
+
+    if (spent === 0 && budgeted === 0) {
+        balanceClass = '';
+        balanceString = '';
+    }
+
+    const activeHandler = () => {
+        setIsActive(!isActive);
+    };
+
+    const displayKebabOptions = () => {
+        console.log('Function separate oh yeah');
+    };
+
+    const toggle = isActive ? (
+        <FontAwesomeIcon icon={faMinus} className={classes.toggle} />
     ) : (
-      <span className={classes.percentage}>
-        {(percent * 100).toFixed(0)}% of Income
-      </span>
+        <FontAwesomeIcon icon={faPlus} className={classes.toggle} />
     );
 
-  let balanceClass = null;
-  let balanceString = null;
+    return (
+        <>
+            <Droppable droppableId={categoryTitle} key={categoryTitle}>
+                {(provided, snapshot) => (
+                    <div
+                        className={`${
+                            snapshot.isDraggingOver && classes.backgroundDrag
+                        }`}
+                    >
+                        <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                        >
+                            <div className={classes.parentContainer}>
+                                <div onClick={activeHandler}>
+                                    <div className={classes.container}>
+                                        <div className={classes.toggle}>
+                                            {toggle}
+                                        </div>
 
-  if (spent === budgeted) {
-    balanceClass = 'balanced';
-    balanceString = `Balanced!`;
-  }
-  if (spent > budgeted) {
-    balanceClass = 'over';
-    let num =
-      (spent - budgeted) % 1 === 0
-        ? spent - budgeted
-        : (spent - budgeted).toFixed(2);
-    balanceString = `$${num.toLocaleString()} Over`;
-  }
+                                        <div>
+                                            <div className={classes.title}>
+                                                {categoryTitle}
+                                                {totalIncome && percentDisplay}
+                                                {!totalIncome && ''}
+                                            </div>
+                                        </div>
 
-  if (spent < budgeted) {
-    balanceClass = 'under';
-    let num =
-      (budgeted - spent) % 1 === 0
-        ? budgeted - spent
-        : +(budgeted - spent).toFixed(2);
-    balanceString = `$${num.toLocaleString()} Under`;
-  }
+                                        <div className={classes.budgetGridItem}>
+                                            <div className={classes.flex}>
+                                                <div className={classes.spent}>
+                                                    <span className={classes.bold}>
+                                                        Spent
+                                                    </span>{' '}
+                                                    ${spent}
+                                                </div>
 
-  if (spent === 0 && budgeted === 0) {
-    balanceClass = '';
-    balanceString = '';
-  }
+                                                <div className={classes.slash}>
+                                                    /
+                                                </div>
 
-  const activeHandler = () => {
-    setIsActive(!isActive);
-  };
+                                                <div className={classes.budgeted}>
+                                                    ${budgeted.toLocaleString()}
+                                                </div>
+                                            </div>
+                                        </div>
 
-  const displayKebabOptions = () => {
-    console.log('Function separate oh yeah');
-  };
+                                        <div>
+                                            <div className={classes[balanceClass]}>
+                                                {balanceString}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-  const toggle = isActive ? (
-    <FontAwesomeIcon icon={faMinus} className={classes.toggle} />
-  ) : (
-    <FontAwesomeIcon icon={faPlus} className={classes.toggle} />
-  );
+                                <button
+                                    className={classes.kebab}
+                                    onClick={displayKebabOptions}
+                                >
+                                    <FontAwesomeIcon icon={faEllipsisH} />
+                                </button>
+                            </div>
 
-  return (
-    <>
-      <Droppable droppableId={categoryTitle} key={categoryTitle}>
-        {(provided, snapshot) => (
-          <div
-            className={`${snapshot.isDraggingOver && classes.backgroundDrag}`}
-          >
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              <div className={classes.parentContainer}>
-                <div onClick={activeHandler}>
-                  <div className={classes.container}>
-                    <div className={classes.toggle}>{toggle}</div>
-                    <div>
-                      <div className={classes.title}>
-                        {categoryTitle}
-                        {totalIncome && percentDisplay}
-                        {!totalIncome && ''}
-                      </div>
-                    </div>
-                    <div className={classes.budgetGridItem}>
-                      <div className={classes.flex}>
-                        <div className={classes.spent}>
-                          <span className={classes.bold}>Spent</span> ${spent}
+                            <ul className={classes.list}>
+                                {isActive && (
+                                    <div className={classes.activeBar}>
+                                        <div className={classes.activeBalanceChip}>
+                                            <div className={classes[balanceClass]}>
+                                                {balanceString}
+                                            </div>
+                                        </div>
+
+                                        <div className={classes.activeFraction}>
+                                            <div className={classes.flex}>
+                                                <div className={classes.spent}>
+                                                    <span className={classes.bold}>
+                                                        Spent
+                                                    </span>{' '}
+                                                    ${spent}
+                                                </div>
+                                                <div className={classes.slash}>
+                                                    /{' '}
+                                                </div>
+                                                <div className={classes.budgeted}>
+                                                    <span className={classes.bold}>
+                                                        Budgeted
+                                                    </span>{' '}
+                                                    ${budgeted}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+
+                                {isActive &&
+                                    // items !== [] &&
+                                    items.map((item, index) => {
+                                        return (
+                                            <BudgetItem
+                                                key={item.id}
+                                                index={index}
+                                                title={item.id}
+                                                date={item.billDate}
+                                                budgetedAmount={
+                                                    item.budgetAmount
+                                                }
+                                                tabID={tabID}
+                                                accordionType={'category'}
+                                            />
+                                        );
+                                    })}
+                                {provided.placeholder}
+                            </ul>
                         </div>
-                        <div className={classes.slash}>/</div>
-                        <div className={classes.budgeted}>
-                          ${budgeted.toLocaleString()}
-                        </div>
-                      </div>
                     </div>
-                    <div>
-                      <div className={classes[balanceClass]}>
-                        {balanceString}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className={classes.kebab} onClick={displayKebabOptions}>
-                  <FontAwesomeIcon icon={faEllipsisH} />
-                </div>
-              </div>
-
-              <ul className={classes.list}>
-                {isActive && (
-                  <div className={classes.activeBar}>
-                    <div className={classes.activeFraction}>
-                      <div className={classes.flex}>
-                        <div className={classes.spent}>
-                          <span className={classes.bold}>Spent</span> ${spent}
-                        </div>
-                        <div className={classes.slash}>/</div>
-                        <div className={classes.budgeted}>
-                          <span className={classes.bold}>Budgeted</span> $
-                          {budgeted}
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className={classes.activeBalanceChip}>
-                      <div className={classes[balanceClass]}>
-                        {balanceString}
-                      </div>
-                    </div>
-                  </div>
                 )}
-
-                {isActive &&
-                  items !== [] &&
-                  items.map((item, index) => {
-                    return (
-                      <BudgetItem
-                        key={item.id}
-                        index={index}
-                        title={item.id}
-                        date={item.billDate}
-                        budgetedAmount={item.budgetAmount}
-                        tabID={tabID}
-                        accordionType={'category'}
-                      />
-                    );
-                  })}
-                {provided.placeholder}
-              </ul>
-            </div>
-          </div>
-        )}
-      </Droppable>
-    </>
-  );
+            </Droppable>
+        </>
+    );
 };
 
 export default CategoryAccordion;
